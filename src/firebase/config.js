068ebfec1aaa567228816b53f89ecd2315edf65a
Oscar Alcazar <oscar.alcazar.lobos@gmail.com>
2023-03-01 20:09:55 -0300

    variables de entorno externalizadas

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
+import { getEnvVariables } from "../helpers/getEnv";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,13 +10,16 @@ import { getFirestore } from "firebase/firestore/lite";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
+const { VITE_APIKEY, VITE_APPID, VITE_AUTHDOMAIN, VITE_MESSAGINGSENDERID, VITE_PROJECTID, VITE_STORAGEBUCKET } = getEnvVariables();
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAAmMN9iWnbAEEpF0uChp5YfsLJfHXZERg",
-  authDomain: "react-projects-1b37d.firebaseapp.com",
-  projectId: "react-projects-1b37d",
-  storageBucket: "react-projects-1b37d.appspot.com",
-  messagingSenderId: "336350538203",
-  appId: "1:336350538203:web:592ed75edddfec185cdffe"
+  apiKey: VITE_APIKEY,
+  authDomain: VITE_AUTHDOMAIN,
+  projectId: VITE_PROJECTID,
+  storageBucket: VITE_STORAGEBUCKET,
+  messagingSenderId: VITE_MESSAGINGSENDERID,
+  appId: VITE_APPID
 };
 
 // Initialize Firebase
---
+++ src/helpers/getEnv.js
@@ -0,0 +1,15 @@
+export const getEnvVariables = () => {
+
+    // import.meta.env;
+
+    return {
+        // ...import.meta.env
+        VITE_APIKEY             : import.meta.env.VITE_APIKEY,
+        VITE_AUTHDOMAIN         : import.meta.env.VITE_AUTHDOMAIN,
+        VITE_PROJECTID          : import.meta.env.VITE_PROJECTID,
+        VITE_STORAGEBUCKET      : import.meta.env.VITE_STORAGEBUCKET,
+        VITE_MESSAGINGSENDERID  : import.meta.env.VITE_MESSAGINGSENDERID,
+        VITE_APPID              : import.meta.env.VITE_APPID,
+    }
+
+}
